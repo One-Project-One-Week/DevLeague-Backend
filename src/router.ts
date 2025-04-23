dff036d5207ad1c0350b40560e01d8d4064f6172
Nyi Zin Thant <nyizinthant@gmail.com>
2025-04-23 16:08:09 +0630

    merge dev and feat/manage-submission

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,6 @@
 import { Router } from 'express';
 import adminAuthRouter from './features/admin/auth/auth.route';
 import adminSubmissionRouter from './features/admin/manage-submission/submission.route';
-import adminAuthMiddleware from './middlewares/adminAuthMiddleware';
-import adminSubmissionRouter from './features/admin/manage-submission/submission.route';
 import hackathonRouter from './features/hackathon/hackathon.routes';
 
 const router = Router();
