bedba5fcbcf3ec6618c9a8dc0b9a71d2fe1c38cc
Paing0 <painghtoomyat007@gmail.com>
2025-04-26 13:32:57 +0630

    fix conflict

--- src/router.ts
+++ src/router.ts
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-import { Router } from 'express';
-import adminAuthRouter from './features/admin/auth/auth.route';
-import adminSubmissionRouter from './features/admin/manage-submission/submission.route';
-import hackathonRouter from './features/hackathon/hackathon.routes';
-import authRouter from './features/auth/auth.routes';
-import adminHackathonRouter from './features/admin/hackathon/hackathon.routes';
-import teamRouter from './features/manage-team/team.routes';
-import leaderboardRouter from './features/leaderboard/leaderboard.routes';
-import hackathonRegisterRouter from './features/hackathon-register/hackathon-register.routes';
-const router = Router();
-router.use('/admin', adminAuthRouter);
-router.use('/auth', authRouter);
-router.use('/hackathons', hackathonRouter);
-router.use('/', hackathonRegisterRouter);
-router.use('/admin', adminSubmissionRouter);
-router.use('/admin/hackathons', adminHackathonRouter);
-router.use('/teams', teamRouter);
-router.use('/leaderboard', leaderboardRouter);
-=======
 import { Router } from "express";
 import adminAuthRouter from "./features/admin/auth/auth.route";
 import adminSubmissionRouter from "./features/admin/manage-submission/submission.route";
@@ -37,6 +17,5 @@ router.use("/admin/hackathons", adminHackathonRouter);
 router.use("/teams", teamRouter);
 router.use("/leaderboard", leaderboardRouter);
 router.use("/user-history", userHistoryRouter);
->>>>>>> feat/user-history
 
 export default router;
